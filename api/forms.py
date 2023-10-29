481347202818a4224cdbb41173ea49eb78b6b3ba
gitbmvb <bruno.martval@gmail.com>
2023-10-29 20:32:31 -0300

    Fazendo adaptações

--- api/forms.py
+++ api/forms.py
@@ -1,9 +1,10 @@
 from django import forms
+from .util import Turma
 
-turmas = [[turma.split(",")[0], turma.split(",")[1].title(), turma.split(",")[2]] for turma in open("turmas.txt", 'r').readlines()]
+turmas = [Turma(turma.split(",")[0], turma.split(",")[1], turma.split(",")[2]) for turma in open("turmas.txt", 'r').readlines()]
 
 class ClassroomsForm(forms.Form):
     choices = forms.MultipleChoiceField(
-        choices=[(i, f"{turmas[i][0]} | {turmas[i][1]} | {turmas[i][2]}") for i in range(len(turmas))],
+        choices=[(i, turmas[i].nome) for i in range(len(turmas))],
         widget=forms.CheckboxSelectMultiple,
     )
